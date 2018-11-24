8929bff7ffee288089f67075874e28219e4a9d2f
Kirk Wight <kwight@kwight.ca>
2018-11-23 23:37:15 -0800

    Allow `site` and `per-page` attributes to `<wp-posts>`.

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 class WPPosts extends HTMLElement {
   async connectedCallback () {
-    const response = await fetch(`https://public-api.wordpress.com/wp/v2/sites/kwight.blog/posts`)
+    const response = await fetch(`https://public-api.wordpress.com/wp/v2/sites/${this.site}/posts?per_page=${this.perPage}`)
     const data = await response.json()
     window.data = data
     this.render(data)
+  }
+
+  get site () {
+    return this.getAttribute('site')
+  }
+
+  get perPage () {
+    return this.getAttribute('per-page') || 10
+  }
+
+  static get observedAttributes () {
+    return ['site', 'per-page']
   }
 
   render (data) {
