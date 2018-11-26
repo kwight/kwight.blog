e6f6871bbe43abc238ddea6dd8d138b839435c35
Kirk Wight <kwight@kwight.ca>
2018-11-25 21:34:33 -0800

    This should keep these varaiable assignments out of the global scope, but I can't tell if it's really working or not.

--- script.js
+++ script.js
@@ -37,8 +37,10 @@ class WPPosts extends HTMLElement {
 }
 window.customElements.define('wp-posts', WPPosts)
 
-const menu = document.getElementById('menu')
-const close = document.getElementById('close')
-const navigation = document.getElementById('menu-content')
-const main = document.getElementById('blog-content')
+{
+  const menu = document.getElementById('menu')
+  const close = document.getElementById('close')
+  const navigation = document.getElementById('menu-content')
+  const main = document.getElementById('blog-content')
 ;[menu, close].forEach((el) => el.addEventListener('click', () => [menu, close, navigation, main].forEach((el) => el.classList.toggle('active'))))
+}
