d765e78f816401f77b11c06ac36ea929c49c1431
Kirk Wight <kwight@kwight.ca>
2018-11-25 19:04:40 -0800

    Add a menu panel.

--- script.js
+++ script.js
@@ -39,4 +39,6 @@ window.customElements.define('wp-posts', WPPosts)
 
 const menu = document.getElementById('menu')
 const close = document.getElementById('close')
-;[menu, close].forEach((el) => el.addEventListener('click', () => [menu, close].forEach((el) => el.classList.toggle('active'))))
+const navigation = document.getElementById('menu-content')
+const main = document.getElementById('blog-content')
+;[menu, close].forEach((el) => el.addEventListener('click', () => [menu, close, navigation, main].forEach((el) => el.classList.toggle('active'))))
