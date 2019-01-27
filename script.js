47cdb1893be9403cfe7cde43f1582a0c84d0d862
Kirk Wight <kwight@kwight.ca>
2019-01-26 16:35:51 -0800

    Move the web component to its own file, taking its template with it.

--- script.js
+++ script.js
@@ -1,46 +1,7 @@
-class WPPosts extends HTMLElement {
-  constructor () {
-    super()
-    this.attachShadow({ mode: 'open' })
-    this.template = document.getElementById('post').content
-  }
-  async connectedCallback () {
-    const response = await fetch(`https://public-api.wordpress.com/wp/v2/sites/${this.site}/posts?per_page=${this.perPage}`)
-    const data = await response.json()
-    window.data = data
-    this.render(data)
-  }
-
-  get site () {
-    return this.getAttribute('site')
-  }
-
-  get perPage () {
-    return this.getAttribute('per-page') || 10
-  }
-
-  static get observedAttributes () {
-    return ['site', 'per-page']
-  }
-
-  render (data) {
-    if (!Array.isArray(data)) {
-      return
-    }
-    data.map(post => {
-      let article = this.template.cloneNode(true)
-      article.querySelector('h1').innerHTML = post.title.rendered
-      article.querySelector('div').innerHTML = post.content.rendered
-      this.shadowRoot.appendChild(article)
-    })
-  }
-}
-window.customElements.define('wp-posts', WPPosts)
-
 {
   const menu = document.getElementById('menu')
   const close = document.getElementById('close')
   const navigation = document.getElementById('menu-content')
   const main = document.getElementById('blog-content')
-;[menu, close].forEach((el) => el.addEventListener('click', () => [menu, close, navigation, main].forEach((el) => el.classList.toggle('active'))))
+        ;[menu, close].forEach((el) => el.addEventListener('click', () => [menu, close, navigation, main].forEach((el) => el.classList.toggle('active'))))
 }
