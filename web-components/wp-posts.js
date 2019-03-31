e9c8684e428c1e2753ea7aa5cc2026fe015448fa
Kirk Wight <kwight@kwight.ca>
2019-03-31 14:16:46 -0700

    Use the fetch utility to populate the component with posts.

--- web-components/wp-posts.js
+++ web-components/wp-posts.js
@@ -1,12 +1,32 @@
 import { LitElement, html } from 'lit-element';
+import { wpcomFetch } from '../utils/wpcom-fetch';
 
 class WPPosts extends LitElement {
+  constructor() {
+    super();
+    this.posts = [];
+  }
+
+  static get properties() {
+    return {
+      site: { type: String },
+      'per-page': { type: Number },
+      posts: { type: Array },
+    };
+  }
+
+  async connectedCallback() {
+    super.connectedCallback();
+    this.posts = await wpcomFetch(this.site, 'posts', { 'per-page': this['per-page'] });
+  }
+
   render() {
-    return html`  
-    <article class="post">
-      <h1 class="post-title"></h1>
-      <div class="post-content"></div>
-    </article>
-  `;
+    return html`
+      ${this.posts.map((post, index) => html`
+      <h1>${post.title.rendered}</h1>
+    `)}
+    `;
   }
 }
+
+customElements.define('wp-posts', WPPosts);
