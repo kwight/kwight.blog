730410a257bff8252ccf32db997e949476de8f3b
Kirk Wight <kwight@kwight.ca>
2019-03-31 10:52:24 -0700

    Add loading "spinner" and `wp-posts`.

--- web-components/wp-posts.js
+++ web-components/wp-posts.js
@@ -4,13 +4,19 @@ class WPPosts extends HTMLElement {
     this.attachShadow({ mode: 'open' })
     this.template = document.createElement('template')
     this.template.innerHTML = getWPPostsTemplate()
+
+    this.state = {
+      posts: {}
+    }
   }
   async connectedCallback() {
     const response = await fetch(`https://public-api.wordpress.com/wp/v2/sites/${this.site}/posts?per_page=${this.perPage}`)
     const data = await response.json()
     // TODO: Remove this eventually
-    window.data = data
+    this.state.posts = data
     this.render(data)
+    document.getElementById('spinner').classList.toggle('active')
+    this.classList.toggle('active')
   }
 
   get site() {
