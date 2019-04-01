b99e7807a44e41219bc7b480563534bb7c7d11d2
Kirk Wight <kwight@kwight.ca>
2019-03-31 23:31:30 -0700

    Improve parameter handling in wpcom.js.

--- utils/wpcom.js
+++ utils/wpcom.js
@@ -1,17 +1,23 @@
 const apiBase = 'https://public-api.wordpress.com/wp/v2'
 
-async function wpcomFetch (site, path, params = {}) {
+async function wpcomFetch (site, path, params) {
   if (!site || !path) {
     return
   }
-  params = new URLSearchParams(params)
-  const response = await fetch(`${apiBase}/sites/${site}/${path}/?${params}`)
+  let url = `${apiBase}/sites/${site}/${path.replace(/\/$/, '')}`
+  if (params) {
+    url = `${url}?${new URLSearchParams(params)}`
+  }
+  const response = await fetch(url)
   return response.json()
 }
 
-function wpcomGetThumbnailUrl (url, params = {}) {
-  params = new URLSearchParams(params)
-  return `${url}/?${params}`
+function wpcomGetThumbnailUrl (url, params) {
+  url = url.replace(/\/$/, '')
+  if (params) {
+    url = `${url}?${new URLSearchParams(params)}`
+  }
+  return url
 }
 
 export { apiBase, wpcomFetch, wpcomGetThumbnailUrl }
