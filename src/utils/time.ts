372c60417ec5b638759cb58b5c2d9447aa8e0337
Kirk Wight <kwight@kwight.ca>
2019-04-06 16:19:38 -0700

    Better formatting for `getHumanReadableTimeStamp`.

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,3 +1,10 @@
-export function getHumanReadableTimestamp(timestamp) {
-    return new Date(timestamp).toLocaleString(undefined, { day: 'numeric', month: 'short', year: 'numeric', hour: 'numeric', minute: 'numeric' });
+export function getHumanReadableTimestamp(timestamp: string) {
+  return new Date(timestamp)
+    .toLocaleString(undefined, {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    });
 }
