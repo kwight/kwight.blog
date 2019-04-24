db9f9b6163c4dbaf5ca3a15896f78a0ecc749c17
Kirk Wight <kwight@kwight.ca>
2019-04-23 21:34:57 -0700

    Remove unnecessary block from the entry file.

--- src/entry.ts
+++ src/entry.ts
@@ -1,18 +1,18 @@
 import { getAttributesByPath } from './lib/util'
 
-{
-  const site = 'kwight.blog'
-  const menu = document.getElementById('menu')
-  const close = document.getElementById('close')
-  const navigation = document.getElementById('menu-content')
-  const main = document.getElementById('blog-content')
-    ;[menu, close].forEach((el) => el!.addEventListener('click', () => [menu, close, navigation, main].forEach((el) => el!.classList.toggle('active'))))
+const site = 'kwight.blog'
 
-  const wpPosts = document.createElement('wp-posts')
-  wpPosts.setAttribute('site', site)
-  const wpPost = document.createElement('wp-post')
-  const attr = getAttributesByPath(location.pathname)
-  if (main) {
-    main.appendChild('list' === attr.view ? wpPosts : wpPost)
-  }
+const menu = document.getElementById('menu')
+const close = document.getElementById('close')
+const navigation = document.getElementById('menu-content')
+const main = document.getElementById('blog-content')
+  ;[menu, close].forEach((el) => el!.addEventListener('click', () => [menu, close, navigation, main].forEach((el) => el!.classList.toggle('active'))))
+
+const wpPosts = document.createElement('wp-posts')
+wpPosts.setAttribute('site', site)
+const wpPost = document.createElement('wp-post')
+const attr = getAttributesByPath(location.pathname)
+
+if (main) {
+  main.appendChild('list' === attr.view ? wpPosts : wpPost)
 }
