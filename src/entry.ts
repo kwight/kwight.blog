e11bd37855b7b1eaf9ce9771d0169bc8e87b6993
Kirk Wight <kwight@kwight.ca>
2019-04-27 12:20:25 -0700

    Get rid of site handling, it's hardcoded now.

--- src/entry.ts
+++ src/entry.ts
@@ -1,7 +1,5 @@
 import { getAttributesByPath } from './lib/util'
 
-const site = 'kwight.blog'
-
 const menu = document.getElementById('menu')
 const close = document.getElementById('close')
 const navigation = document.getElementById('menu-content')
@@ -9,7 +7,6 @@ const main = document.getElementById('blog-content')
   ;[menu, close].forEach((el) => el!.addEventListener('click', () => [menu, close, navigation, main].forEach((el) => el!.classList.toggle('active'))))
 
 const wpPosts = document.createElement('wp-posts')
-wpPosts.setAttribute('site', site)
 const wpPost = document.createElement('wp-post')
 const attr = getAttributesByPath(location.pathname)
 
