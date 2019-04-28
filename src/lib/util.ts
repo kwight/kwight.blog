e5c4f48835039a6cc19f31f3a7a989561dcd6c36
Kirk Wight <kwight@kwight.ca>
2019-04-28 13:43:50 -0700

    Change `getAttributesBypath` to `getParamsByPath`, based on the `wpcomParams` interface.

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,3 +1,5 @@
+import { WPcomParams } from './wpcom';
+
 export function getHumanReadableTimestamp(timestamp: string): string {
   return new Date(timestamp)
     .toLocaleString(undefined, {
@@ -9,19 +11,20 @@ export function getHumanReadableTimestamp(timestamp: string): string {
     });
 }
 
-interface AttributesByPath {
-  'view': string,
-  'slug': string | undefined,
-}
-
-export function getAttributesByPath(path: string): AttributesByPath {
-  switch (path) {
-    case '/2018/08/15/til-auto-input-yes-no-anything-to-command-prompts':
-    case '/2018/02/11/how-facebook-is-killing-insert-creative-income-here-and-why-i-give-money-to-amanda-palmer':
-    case '/2017/10/09/installing-xdebug-on-macos-high-sierra':
-      return { 'view': 'single', 'slug': path.match(/[0-9a-z-%]*$/)!.shift() }
-    case '/':
+export function getParamsByPath(path: string): WPcomParams {
+  const baseParams = {
+    site: 'kwight.blog',
+    path: 'posts',
+  }
+  switch (true) {
+    case '/2018/08/15/til-auto-input-yes-no-anything-to-command-prompts' === path:
+    case '/2018/02/11/how-facebook-is-killing-insert-creative-income-here-and-why-i-give-money-to-amanda-palmer' === path:
+    case '/2017/10/09/installing-xdebug-on-macos-high-sierra' === path:
+      return { ...baseParams, 'slug': path.match(/[0-9a-z-%]*$/)!.shift() }
+    case /\/search\/[0-9a-z\+]/.test(path):
+      return { ...baseParams, 'search': path.match(/[0-9a-z-+%]*$/)!.shift() }
+    case '/' === path:
     default:
-      return { 'view': 'list', 'slug': undefined }
+      return baseParams
   }
 }
