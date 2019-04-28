ad16d09ac031f47b2d60d6fc253da50163809d54
Kirk Wight <kwight@kwight.ca>
2019-04-28 11:10:00 -0700

    Use an interface to define parameters for the `wpcom` library.

--- src/lib/wpcom.ts
+++ src/lib/wpcom.ts
@@ -1,23 +1,31 @@
-const apiBase = 'https://public-api.wordpress.com/wp/v2'
+export const apiBase = 'https://public-api.wordpress.com/wp/v2'
 
-async function wpcomFetch(site: string, path: string, params?: {}) {
-  if (!site || !path) {
-    return
-  }
+export interface WPcomParams {
+  site: string,
+  path: string,
+  slug?: string,
+  search?: string,
+}
+
+export async function wpcomFetch(params: WPcomParams) {
+  const { site, path, slug, search } = params
+
   let url = `${apiBase}/sites/${site}/${path.replace(/\/$/, '')}`
-  if (params) {
-    url = `${url}?${new URLSearchParams(params)}`
+  if (slug) {
+    url = `${url}/slug:${params.slug}`
+  } else if (search) {
+    url = `${url}?${new URLSearchParams(search)}`
   }
+
   const response = await fetch(url)
+
   return response.json()
 }
 
-function wpcomGetThumbnailUrl(url: string, params: {}) {
+export function wpcomGetThumbnailUrl(url: string, params: {}) {
   url = url.replace(/\/$/, '')
   if (params) {
     url = `${url}?${new URLSearchParams(params)}`
   }
   return url
 }
-
-export { apiBase, wpcomFetch, wpcomGetThumbnailUrl }
