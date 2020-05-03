a046673478ce7b1d927e97bf9c817f643f479754
Kirk Wight <kwight@kwight.ca>
2020-05-03 11:33:37 -0700

    Get toggled menu going.

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,19 @@
 import 'dart:html';
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  final menu = querySelector('#menu');
+  final close = querySelector('#close');
+  final navigation = querySelector('#menu-content');
+  final main = querySelector('#blog-content');
+  final spinner = querySelector('#spinner');
+
+  void toggleMenu(Event _) {
+    for (final element in [menu, close, main, navigation]) {
+      element.classes.toggle('active');
+    }
+  }
+
+  for (final element in [menu, close]) {
+    element.onClick.listen(toggleMenu);
+  }
 }
