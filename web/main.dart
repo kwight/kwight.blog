8a3635b1cba13def6b86378c9455c6c3e62f0b0b
Kirk Wight <kwight@kwight.ca>
2020-05-04 08:24:07 -0700

    Sort out the global elements.

--- web/main.dart
+++ web/main.dart
@@ -1,19 +1,19 @@
 import 'dart:html';
 
+final Element menu = querySelector('#menu');
+final Element close = querySelector('#close');
+final Element menuContent = querySelector('#menu-content');
+final Element blogContent = querySelector('#blog-content');
+final Element spinner = querySelector('#spinner');
+
 void main() {
-  final menu = querySelector('#menu');
-  final close = querySelector('#close');
-  final navigation = querySelector('#menu-content');
-  final main = querySelector('#blog-content');
-  final spinner = querySelector('#spinner');
-
-  void toggleMenu(Event _) {
-    for (final element in [menu, close, main, navigation]) {
-      element.classes.toggle('active');
-    }
-  }
-
   for (final element in [menu, close]) {
     element.onClick.listen(toggleMenu);
+  }
+}
+
+void toggleMenu(Event _) {
+  for (final element in [menu, close, menuContent, blogContent]) {
+    element.classes.toggle('active');
   }
 }
