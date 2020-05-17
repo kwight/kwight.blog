19dc1d8502bf5daa31bb6eb56ecfba09bb77941e
Kirk Wight <kwight@kwight.ca>
2020-05-17 15:09:41 -0700

    Add a WPcom class with common WP REST API methods.

--- web/main.dart
+++ web/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'dart:html';
+import 'package:meta/meta.dart';
 
 final Element menu = querySelector('#menu');
 final Element close = querySelector('#close');
@@ -10,10 +12,48 @@ void main() {
   for (final element in [menu, close]) {
     element.onClick.listen(toggleMenu);
   }
+  final wpcom = WPcom(site: 'kwight.blog');
+  wpcom.getPosts();
 }
 
 void toggleMenu(Event _) {
   for (final element in [menu, close, menuContent, blogContent]) {
     element.classes.toggle('active');
+  }
+}
+
+class WPcom {
+  WPcom({@required this.site}) {
+    baseUrl = 'https://public-api.wordpress.com/wp/v2/sites/$site';
+  }
+  final String site;
+  String baseUrl;
+
+  Future<List<Object>> getPosts() => makeGetRequest('$baseUrl/posts');
+
+  Future<List<Object>> getCategories() => makeGetRequest('$baseUrl/categories');
+
+  Future<List<Object>> getCategoryPosts(int id) =>
+      makeGetRequest('$baseUrl/posts?categories=$id');
+
+  Future<List<Object>> getTags() => makeGetRequest('$baseUrl/tags');
+
+  Future<List<Object>> getTagPosts(int id) =>
+      makeGetRequest('$baseUrl/posts?tags=$id');
+
+  Future<List<Object>> search(String term) =>
+      makeGetRequest('$baseUrl/search?search=$term');
+
+  Future<List<Object>> makeGetRequest(String url) async {
+    try {
+      final response = await HttpRequest.getString(url);
+      return jsonDecode(response) as List<Object>;
+    } on Exception catch (error) {
+      return [
+        {
+          'error': error.toString(),
+        }
+      ];
+    }
   }
 }
