13b537478d26911849b73f73ddc2e334f4980cdd
Kirk Wight <kwight@kwight.ca>
2021-02-07 18:57:20 -0800

    Hang common fetch functions on `window` at `kwightBlog`.

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,7 @@
-import 'dart:convert';
 import 'dart:html';
-import 'package:meta/meta.dart';
+import 'package:js/js.dart';
+import 'lib/window.dart';
+import 'lib/wpcom.dart';
 
 final Element menu = querySelector('#menu');
 final Element close = querySelector('#close');
@@ -12,48 +13,19 @@ void main() {
   for (final element in [menu, close]) {
     element.onClick.listen(toggleMenu);
   }
-  final wpcom = WPcom(site: 'kwight.blog');
-  wpcom.getPosts();
+
+  final wpcom = WPcom(site: 'en.blog.wordpress.com');
+  kwightBlog = KwightBlog(
+      getPosts: allowInterop(wpcom.getPosts),
+      getCategories: allowInterop(wpcom.getCategories),
+      getCategoryPosts: allowInterop(wpcom.getCategoryPosts),
+      getTags: allowInterop(wpcom.getTags),
+      getTagPosts: allowInterop(wpcom.getTagPosts),
+      search: allowInterop(wpcom.search));
 }
 
 void toggleMenu(Event _) {
   for (final element in [menu, close, menuContent, blogContent]) {
     element.classes.toggle('active');
-  }
-}
-
-class WPcom {
-  WPcom({@required this.site}) {
-    baseUrl = 'https://public-api.wordpress.com/wp/v2/sites/$site';
-  }
-  final String site;
-  String baseUrl;
-
-  Future<List<Object>> getPosts() => makeGetRequest('$baseUrl/posts');
-
-  Future<List<Object>> getCategories() => makeGetRequest('$baseUrl/categories');
-
-  Future<List<Object>> getCategoryPosts(int id) =>
-      makeGetRequest('$baseUrl/posts?categories=$id');
-
-  Future<List<Object>> getTags() => makeGetRequest('$baseUrl/tags');
-
-  Future<List<Object>> getTagPosts(int id) =>
-      makeGetRequest('$baseUrl/posts?tags=$id');
-
-  Future<List<Object>> search(String term) =>
-      makeGetRequest('$baseUrl/search?search=$term');
-
-  Future<List<Object>> makeGetRequest(String url) async {
-    try {
-      final response = await HttpRequest.getString(url);
-      return jsonDecode(response) as List<Object>;
-    } on Exception catch (error) {
-      return [
-        {
-          'error': error.toString(),
-        }
-      ];
-    }
   }
 }
